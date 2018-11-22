0f952ce52117b29a7e488d1a24d3d7db4bfabc98
Direct2Guests <sean@direct2guests.com>
2018-11-22 08:31:43 +0800

    Update ffmpeg-autoscript.sh

--- ffmpeg-autoscript.sh
+++ ffmpeg-autoscript.sh
@@ -96,7 +96,7 @@ cd ffmpeg
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" 
 ./configure --prefix="$HOME/ffmpeg_build" --pkg-config-flags="--static" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --extra-libs="-lpthread -lm" \
 	--bindir="$HOME/bin" --enable-gpl --enable-libaom --enable-libass --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx \
-	--enable-libx264 --enable-libx265 --enable-nonfree 
+	--enable-libx264 --enable-libx265 --enable-nonfree --enable-openssl
 PATH="$HOME/bin:$PATH" 
 make
 make install

cb8bcceb49d9c057d2a93f6f6a03d4e7e658accc
lms1206 <lms1206@outlook.com>
2018-11-29 21:02:00 +0800

    Add files via upload
    修复Ubuntu默认用户的提权问题,和一些拼写错误等

--- ffmpeg-autoscript.sh
+++ ffmpeg-autoscript.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
+echo "#######################################"
+echo "#######################################"
+echo "    Please Enter Your Root Password"
+sudo ls
+
 
 echo "---------------------------------------"
 echo "#######################################"
 echo "#######################################"
 echo "##          Starting Install         ##"
+echo "##       For Ubuntu 18.04.1 LTS      ##"
 echo "## Copyright @github.com/lazerbeam84 ##"
 echo "#######################################"
 echo "#######################################"
 echo "---------------------------------------"
+
+echo 
 
 echo "---------------------------------------"
 echo "######################"
@@ -15,8 +23,8 @@ echo "## Installing preps ##"
 echo "######################"
 echo "---------------------------------------"
 
-apt-get update -y
-apt-get -y dist-upgrade
+sudo apt update 
+sudo apt -y dist-upgrade
 echo "---------------------------------------"
 echo "---------------------------------------"
 echo "#"
@@ -38,9 +46,9 @@ echo "#############################"
 echo "## Installing Dependencies ##"
 echo "#############################"
 echo "---------------------------------------"
-echo "---------------------------------------"openssl
+echo "---------------------------------------"
 
-apt-get -y install autoconf automake build-essential git asciidoc libgnutls28-dev cmake git-core libass-dev libfreetype6-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev  libxcb-shm0-dev libssl-dev libxcb-xfixes0-dev pkg-config texinfo wget zlib1g-dev libx264-dev yasm libx265-dev libnuma-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev libzthread-2.3-2 libzthread-dev doxygen
+sudo apt-get -y install autoconf automake build-essential git asciidoc libgnutls28-dev cmake git-core libass-dev libfreetype6-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev  libxcb-shm0-dev libssl-dev libxcb-xfixes0-dev pkg-config texinfo wget zlib1g-dev libx264-dev yasm libx265-dev libnuma-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev libzthread-2.3-2 libzthread-dev doxygen openssl
 
 echo "---------------------------------------"
 echo "#######################"
@@ -57,10 +65,10 @@ echo "---------------------------------------"
 echo "---------------------------------------"
 mkdir -p ~/ffmpeg_sources ~/bin
 cd ~/ffmpeg_sources
-wget https://www.nasm.us/pub/nasm/releasebuilds/2.13.03/nasm-2.13.03.tar.bz2
-tar xjvf nasm-2.13.03.tar.bz2
-rm -rf nasm-2.13.03.tar.bz2
-cd nasm-2.13.03
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.14rc16/nasm-2.14rc16.tar.bz2
+tar xjvf nasm-2.14rc16.tar.bz2
+rm -rf nasm-2.14rc16.tar.bz2
+cd nasm-2.14rc16
 ./autogen.sh
 PATH="$HOME/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
 make
@@ -74,13 +82,14 @@ echo "###############################"
 echo "---------------------------------------"
 
 echo "---------------------------------------"
-mkdir -p ~/ffmpeg_sources/libaom
+mkdir -p ~/ffmpeg_sources/libaom/source
+cd ~/ffmpeg_sources/libaom/source
+git clone https://github.com/mozilla/aom
 cd ~/ffmpeg_sources/libaom
-git clone https://aomedia.googlesource.com/aom
-cmake ./aom
+cmake source
 make
 make install
-ldconfig -v
+sudo ldconfig -v
 echo "---------------------------------------"
 
 echo "---------------------------------------"
@@ -91,17 +100,21 @@ echo "---------------------------------------"
 cd ~/ffmpeg_sources
 wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjvf ffmpeg-snapshot.tar.bz2
-rm -rf ffmpeg-snapshot.tar.bz2cd 
+rm -rf ffmpeg-snapshot.tar.bz2
 cd ffmpeg
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" 
+
 ./configure --prefix="$HOME/ffmpeg_build" --pkg-config-flags="--static" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --extra-libs="-lpthread -lm" \
 	--bindir="$HOME/bin" --enable-gpl --enable-libaom --enable-libass --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx \
 	--enable-libx264 --enable-libx265 --enable-nonfree --enable-openssl
+
 PATH="$HOME/bin:$PATH" 
+
 make
 make install
-ldconfig -v
-hash -r
+sudo ldconfig -v
+sudo hash -r
+
 echo "---------------------------------------"
 
 echo "---------------------------------------"
@@ -110,7 +123,7 @@ echo "## Building Documentation ##"
 echo "############################"
 echo "---------------------------------------"
 
-echo "MANPATH_MAP $HOME/bin $HOME/ffmpeg_build/share/man" >> ~/.manpath
+sudo echo "MANPATH_MAP $HOME/bin $HOME/ffmpeg_build/share/man" >> ~/.manpath
 echo "HTML formatted documentation is available in ~/ffmpeg_build/share/doc/ffmpeg."
 
 echo "---------------------------------------"
@@ -121,11 +134,11 @@ echo "---------------------------------------"
 cd ~
 source ~/.profile
 source ~/.bashrc
-cp ~/bin/ffmpeg /usr/bin/
-cp ~/bin/ffplay /usr/bin/
-cp ~/bin/ffprobe /usr/bin/
+sudo cp ~/bin/ffmpeg /usr/bin/
+sudo cp ~/bin/ffplay /usr/bin/
+sudo cp ~/bin/ffprobe /usr/bin/
 
-ffplay http://www.wowza.com/_h264/BigBuckBunny_115k.mov
+## ffplay http://www.wowza.com/_h264/BigBuckBunny_115k.mov
 
 echo "---------------------------------------"
 echo "---------------------------------------"
